aaf4163831eebc7cf521d0f39d7405abbef617d7
Jesse Pinho <jesse@jessepinho.com>
2012-09-30 01:16:01 -0500

    Revert "Removed unneeded code."
    This reverts commit e1914824e0e5680f6a20e368880057b18f3d633b.

--- admin_toggle.js
+++ admin_toggle.js
@@ -12,7 +12,38 @@ Drupal.behaviors.adminToggle = {
 };
 
 function adminToggle() {
-  $(Drupal.settings.adminToggle.selector).toggle();
+  if (typeof adminToggle.flag == 'undefined' ) {
+    adminToggle.flag = true;
+  }
+
+  // Define elements that may contain only toggle-able items, so that they can
+  // also be hidden when all their children are hidden.
+  var elements = [
+    '.action-links',
+    '.block.block-system.block-menu'
+  ];
+
+  if (adminToggle.flag) {
+    $(Drupal.settings.adminToggle.selector).hide(0, function() {
+      for (i in elements) {
+        if ($(elements[i]).not(':has(a:visible)').length) {
+          $(elements[i]).hide();
+        }
+      }
+    });
+  }
+  else {
+    $(Drupal.settings.adminToggle.selector).show(0, function() {
+      for (i in elements) {
+        if ($(elements[i]).filter(':not(:has(a:visible))').length) {
+          console.log($(elements[i]).filter(':not(:has(a:visible))'));
+          $(elements[i]).show();
+        }
+      }
+    });
+  }
+
+  adminToggle.flag = !adminToggle.flag;
 }
 
 })(jQuery);
