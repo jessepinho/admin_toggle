cb9c9bf51418b415801475ef357e7654b2745f8e
Jesse Pinho <jesse@jessepinho.com>
2012-09-30 00:44:18 -0500

    Commit of code I probably won't keep, just so I can reference it if necessary.

--- admin_toggle.js
+++ admin_toggle.js
@@ -5,11 +5,46 @@ Drupal.behaviors.adminToggle = {
     $(document).keypress(function(e) {
       var unicode = e.keyCode ? e.keyCode : e.charCode;
       if (String.fromCharCode(unicode) == Drupal.settings.adminToggle.key) {
-        $(Drupal.settings.adminToggle.selector).slideToggle('fast');
+        adminToggle();
       }
     });
   }
 };
+
+function adminToggle() {
+  if (typeof adminToggle.flag == 'undefined' ) {
+    adminToggle.flag = true;
+  }
+
+  // Define elements that may contain only toggle-able items, so that they can
+  // also be hidden when all their children are hidden.
+  var elements = [
+    '.action-links',
+    '.block.block-system.block-menu'
+  ];
+
+  if (adminToggle.flag) {
+    $(Drupal.settings.adminToggle.selector).hide(0, function() {
+      for (i in elements) {
+        if ($(elements[i]).not(':has(a:visible)').length) {
+          $(elements[i]).hide();
+        }
+      }
+    });
+  }
+  else {
+    $(Drupal.settings.adminToggle.selector).show(0, function() {
+      for (i in elements) {
+        if ($(elements[i]).filter(':not(:has(a:visible))').length) {
+          console.log($(elements[i]).filter(':not(:has(a:visible))'));
+          $(elements[i]).show();
+        }
+      }
+    });
+  }
+
+  adminToggle.flag = !adminToggle.flag;
+}
 
 })(jQuery);
 
