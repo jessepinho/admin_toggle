e00aee32e4a6a1d34b4880966238e4b169d42050
Jesse Pinho <jesse@jessepinho.com>
2012-09-29 22:41:46 -0500

    Added JS for toggling admin elements.

--- admin_toggle.js
+++ admin_toggle.js
@@ -2,6 +2,12 @@
 
 Drupal.behaviors.adminToggle = {
   attach: function(context, settings) {
+    $(document).keypress(function(e) {
+      var unicode = e.keyCode? e.keyCode : e.charCode;
+      if (String.fromCharCode(unicode) == Drupal.settings.adminToggle.key) {
+        $(Drupal.settings.adminToggle.selector).slideToggle('fast');
+      }
+    });
   }
 };
 
